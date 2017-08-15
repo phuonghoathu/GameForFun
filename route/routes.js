65f163fcaf84554b80cedb82277e87996f9e0372
phuonghoathu <phuonghoathu@gmail.com>
2017-08-15 22:14:28 +0700

    create first1

--- route/routes.js
+++ route/routes.js
@@ -4,20 +4,12 @@ var express = require('express');
 // create router object
 var router = express.Router();
 
-
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser'); //connects bodyParsing middleware
-var formidable = require('formidable');
-var path = require('path');     //used for file path
-var fs =require('fs-extra');    //File System-needed for renaming file etc
 var slidersModel = mongoose.model('sliderbars');
 var gamesModel = mongoose.model('games');
 var listGames;
 var listSliders;
 var message;
-var app = express();
-app.use(express.static(path.join(__dirname, 'uploads')));
-
 
 // route for homepage
 router.get('/', function (req, res) {
@@ -73,15 +65,7 @@ router.post('/addSlider', function(req, res) {
 });
 
 // Add new game to DB
-app.use(bodyParser({defer: true}));
- app.route('/addGame').post(function(req, res) {
-	var form = new formidable.IncomingForm();
-    //Formidable uploads to operating systems tmp dir by default
-    form.uploadDir = "./imgs";       //set upload directory
-    form.keepExtensions = true;     //keep file extension
-  // parse the incoming request containing the form data
-  console.log(form.uploadDir);
-  form.parse(req);
+router.post('/addGame', function(req, res) {
 	new gamesModel({thumbnail: req.body.thumbnail,
 				name: req.body.name,
 				url: req.body.url})
